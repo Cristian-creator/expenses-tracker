3e30a3a94bd2b472185adf612ea51aed6c6745cb
cristian <preduscacristian@gmail.com>
2020-04-07 14:08:15 +0300

    added client

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Header from './components/Header';
+import './App.css';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
+export class App extends Component {
+  render() {
+    return (
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    )
+  }
+}
+
+export default App
